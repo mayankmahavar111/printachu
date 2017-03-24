890ab49538166ac3bee4faee9f035d20c6c0992b
mayankmahavar111 <mayankmahavar111@gmail.com>
2017-03-24 22:20:40 +0530

    mayank, 24 th march

--- poster/forms.py
+++ poster/forms.py
@@ -1,14 +1,15 @@
 from django import forms
+from django.contrib.auth.forms import format_html
 from django.contrib.auth.models import User
 
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
-    email=forms.EmailField(required=True,label='Your Email ID: - ',initial='email')
+    email=forms.EmailField(required=True,label='Email ID: - ',initial='email')
     first_name=forms.CharField(required=True,label='Your Name:')
-    username=forms.CharField(required=True,label='Usesrname :')
+    username=forms.CharField(required=True,label='Usesrname ',initial='username')
     dob=forms.DateField(required=True,label='Date of Birth')
-    gender=forms.CharField(required=True,label="Gender : ")
+    gender=forms.CharField(required=True,label="Gender ")
 
     class Meta:
         model=User
