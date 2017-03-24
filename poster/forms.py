dc7f7b8e86e7e340e090ebe17c8cf5a24a0eef7d
mayankmahavar111 <mayankmahavar111@gmail.com>
2017-03-24 17:50:24 +0530

    24 march 2017 mayank -2

---
+++ poster/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm
+
+class RegistrationForm(UserCreationForm):
+    email=forms.EmailField(required=True,label='Your Email ID: - ',initial='email')
+    first_name=forms.CharField(required=True,label='Your Name:')
+    username=forms.CharField(required=True,label='Usesrname :')
+    dob=forms.DateField(required=True,label='Date of Birth')
+    gender=forms.CharField(required=True,label="Gender : ")
+
+    class Meta:
+        model=User
+        fields=(
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2'
+
+        )
+
