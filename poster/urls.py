e99ddccdffb18782708643f6df72411517cdac0d
mayankmahavar111 <mayankmahavar111@gmail.com>
2017-04-04 20:39:46 +0530

    register user

--- poster/urls.py
+++ poster/urls.py
@@ -6,7 +6,9 @@ urlpatterns=[
     url(r'^$',views.Index.as_view(), name='index'),
     url(r'login/$',login,{'template_name': 'poster/login.html'}),
     url(r'logout/$',logout,{'template_name': 'poster/logout.html'}),
-    url(r'register/$', views.register, name='register'),
+    #url(r'register/$', views.register, name='register'),
     url(r'^profile/$', views.Profile.as_view()),
-    url(r'^test/$',views.test)
+    url(r'^test/(?P<your_name>[a-zA-z]{1,10})/(?P<last_name>[a-zA-z]{1,10})/$',views.test),
+    url(r'^test2/$',views.test2),
+    url(r'^register/$',views.saveRegister,name='saveRegister')
 ]
--- poster/views.py
+++ poster/views.py
@@ -8,25 +8,46 @@ from poster.forms import RegistrationForm
 from django.views.generic import TemplateView
 from django.views import  generic
 from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
 
+def saveRegister(request):
+	if request.method == "POST":
+		print request.POST
+		first_name=request.POST.get('firstname')
+		last_name=request.POST.get('lastname')
+		email=request.POST.get('email')
+		password1=request.POST.get('password1')
+		password2=request.POST.get('password2')
+		dob=request.POST.get('dateofbirth')
+		gender=request.POST.get('gender')
+		type=request.POST.get('usertype')
+		User.objects.create(
+			username= first_name,
+			email=email,
+			password=password1,
+		)
+		print first_name,last_name,email,password1,password2,dob,gender,type
+		return redirect('/poster/test2')
+	else:
+		print "hello world"
+		return render(request,'poster/register.html')
 
-def test(request):
-	if request.method == "GET":
+def test2(request):
+	if request.method == "POST":
+		print request.POST
+		x=request.POST.get('x')
+		y=request.POST.get('y')
+		print x,y
+		return redirect('/poster')
+	else:
 		return render(request,'poster/test.html')
 
+def test(request,your_name,last_name):
+
+		print your_name,last_name
+		return redirect('/poster/test2')
 def register(request):
-	print 'Hello World'
-	if request.method=='POST':
-		form=RegistrationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('/poster/login')
-		else:
-			return redirect('/poster/register')
-	else :
-		form=RegistrationForm()
-		args={'form':form}
-		return render(request,'poster/register.html',args)
+		return render(request,'poster/register.html')
 
 @login_required
 def profile(request):
