3fcca2aedca8a45c28251700010b41cab36f75e3
mayankmahavar111 <mayankmahavar111@gmail.com>
2017-06-18 14:54:44 +0530

    profile-back

--- poster/admin.py
+++ poster/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from poster.models import UserProfile,ArtistProfile,CustomerProfile,poster,Category
+from poster.models import UserProfile,ArtistProfile,CustomerProfile,poster,Category,tags
 
 admin.site.register(UserProfile)
 admin.site.register(ArtistProfile)
 admin.site.register(Category)
 admin.site.register(poster)
 admin.site.register(CustomerProfile)
+admin.site.register(tags)
